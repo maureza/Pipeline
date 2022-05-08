8faca155277135d8b55cd607ed5a56757acdce0a
Mauricio <rezamar555@gmail.com>
2022-05-08 16:26:54 -0500

    Opening new branch, called groovy

---
+++ script.groovy
@@ -0,0 +1,14 @@
+def buildApp(){
+  echo 'Building the application...'
+}
+
+def testApp(){
+  echo 'Building the application...'
+}
+
+def deployApp(){
+  echo 'Building the application...'
+  echo "deploying version ${VERSION}"
+}
+
+return this
